ca792c532d4c9474ef096f554eb05cdfa9a9d22f
aadesh_shetty <aadeshshetty123@gmail.com>
2023-02-23 19:08:30 +0530

    all-events and dropdown in navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllEventsComponent } from './all-events/all-events.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'all-events',
+    component: AllEventsComponent,
     pathMatch: 'full',
   }
 ];
--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ResolveEnd, Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  color='rgba(255, 255, 255, 0.55)';
+  constructor(private router:Router) {
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe((routerData) => {
+      if(routerData instanceof ResolveEnd){ 
+         if(routerData.url.includes('all-events')|| routerData.url.includes('upcoming-events')||routerData.url.includes('current-events')){
+           this.color='white';
+         }
+    } 
+})
+    
+  }
   // onSearchChange() {
   //   this.searchText.emit(this.enteredSearch);
   // }
