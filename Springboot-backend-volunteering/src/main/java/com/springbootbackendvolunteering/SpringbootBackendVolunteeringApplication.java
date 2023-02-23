dfc79a87c1f6ec693f4b44d7f897e49d5fed02da
shyam kumar <shyamkumarhasti@gmail.com>
2023-02-23 12:06:56 +0530

    spring boot project created

---
+++ Springboot-backend-volunteering/src/main/java/com/springbootbackendvolunteering/SpringbootBackendVolunteeringApplication.java
@@ -0,0 +1,13 @@
+package com.springbootbackendvolunteering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendVolunteeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendVolunteeringApplication.class, args);
+	}
+
+}
---
+++ Springboot-backend-volunteering/src/test/java/com/springbootbackendvolunteering/SpringbootBackendVolunteeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootbackendvolunteering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendVolunteeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
